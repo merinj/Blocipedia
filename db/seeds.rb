1ba6753cddcb15479bd0938d31879bc94dc1d6e7
Merin Joseph <merinsony18@gmail.com>
2017-10-11 23:15:18 -0700

    seed data partially done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+ # Create Users
+ 5.times do
+   User.create!(
+   email:  Faker::Internet.email,
+   password: 'hellowiki'
+   )
+ end
+ users = User.all
+
+  # Create Wikis
+ 15.times do
+  Wiki.create!(
+     title: Faker::Lorem.word,
+     body:  Faker::Lorem.paragraph
+   )
+ end
+wikis = Wiki.all
+
+puts "Seed finished"
+ puts "#{User.count} users created"
+ puts "#{Wiki.count} wikis created"
